69b5ffb6ba3062f8a54ca88035ae2f96e6519098
Guillermo Rivas <grivas@pop-os.localdomain>
2023-02-17 08:48:37 -0600

    push inicial

---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,24 @@
+// estamos usando .NET
+// aquí "importamos" namespaces
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+
+    // ciclo de vida / lifecycle
+    // - existen métodos que se invocan en momentos específicos de la vida del script
+    
+    void Awake(){}
+
+    // Start is called before the first frame update
+    void Start(){}
+
+    // Update is called once per frame
+    void Update(){}
+
+    void FixedUpdate(){}
+
+    void LateUpdate(){}
+}
